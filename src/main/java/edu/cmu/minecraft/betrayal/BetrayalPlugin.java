796885cbfc7de5350c145706180ac93693e38585
Terence Sun <tsun1215@gmail.com>
2016-11-10 23:12:50 -0500

    Fixed NPE bug when interacting with air

--- src/main/java/edu/cmu/minecraft/betrayal/BetrayalPlugin.java
+++ src/main/java/edu/cmu/minecraft/betrayal/BetrayalPlugin.java
@@ -1,11 +1,14 @@
 package edu.cmu.minecraft.betrayal;
 
-import org.bukkit.Chunk;
+import java.util.logging.Logger;
+
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import edu.cmu.minecraft.betrayal.commands.EnterWorldCommand;
@@ -19,6 +22,7 @@ public class BetrayalPlugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(new DoorListener(this), this);
 		this.getCommand(EnterWorldCommand.COMMAND_NAME).setExecutor(new EnterWorldCommand(this));
 	}
 	@Override
@@ -26,16 +30,35 @@ public class BetrayalPlugin extends JavaPlugin {
 		return new FlatLandGenerator(this);
 	}
 	
-	@EventHandler
-	public void onPlayerDoorOpen(PlayerInteractEvent event) {
-		Action action = event.getAction();
-		Block block = event.getClickedBlock();
-		Blueprint b = RoomManager.getInstance().getBlueprint(block.getChunk());
-		if (action == Action.RIGHT_CLICK_BLOCK) {
-			Entrance door = b.getDoorByBlock(block);
-			if (door != null) {
-				door.open(); //Generate room here?
-				this.getLogger().info("Clicked on door");
+	public static class DoorListener implements Listener {
+		private Plugin plugin;
+		private Logger logger;
+		
+		public DoorListener(Plugin plugin) {
+			this.plugin = plugin;
+			this.logger = this.plugin.getLogger();
+		}
+		
+		// TODO: Extract this class and also maybe migrate to PlayerInteractEntityEvent
+		@EventHandler
+		public void onPlayerDoorOpen(PlayerInteractEvent event) {
+			Action action = event.getAction();
+			Block block = event.getClickedBlock();
+			if (block != null) {
+				logger.info("Block: " + block);
+				logger.info("Chunk: " + block.getChunk());
+				Blueprint b = RoomManager.getInstance().getBlueprint(block.getChunk());
+				if (b != null) {
+					if (action == Action.RIGHT_CLICK_BLOCK) {
+						Entrance door = b.getDoorByBlock(block);
+						if (door != null) {
+							door.open(); //Generate room here?
+							this.logger.info("Clicked on door");
+						}
+					}				
+				} else {
+					this.logger.info("Unable to find chunk at: " + block.getChunk());
+				}
 			}
 		}
 	}
