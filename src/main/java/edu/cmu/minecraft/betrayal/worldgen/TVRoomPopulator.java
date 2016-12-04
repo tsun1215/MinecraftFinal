3a3a8f20af9848b665c54d1944832e8482641653
soctar <soctar@gmail.com>
2016-12-04 16:47:26 -0500

    TV room generator works

--- src/main/java/edu/cmu/minecraft/betrayal/worldgen/TVRoomPopulator.java
+++ src/main/java/edu/cmu/minecraft/betrayal/worldgen/TVRoomPopulator.java
@@ -28,46 +28,31 @@ public class TVRoomPopulator implements FurniturePopulator {
 	
 	@Override
 	public void populate(Blueprint blueprint) {
-		if (blueprint.getWestWalls().size() < 1 || blueprint.getNorthWalls().size() < 1) {
-			if (blueprint.getEastWalls().size() < 1 || blueprint.getSouthWalls().size() < 1) {
-				logger.info("No walls in room??");
-				return;
-			}
-			Wall east = blueprint.getEastWalls().get(0);
-			int baseY = east.getLowCorner().getBlockY()-1;
-			int ceilY = east.getHighCorner().getBlockY();
-			int height = ceilY - baseY - 1;
-			logger.info("baseY = " + baseY + "; ceilY = " + ceilY);
-			int maxZ = Math.max(east.getLowCorner().getBlockZ(), east.getHighCorner().getBlockZ());
-			int minZ = Math.min(east.getLowCorner().getBlockZ(), east.getHighCorner().getBlockZ());
-			int width = maxZ - minZ - 1;
-			int midZ = ((maxZ - minZ) / 2) + minZ;
-			
+		Wall side;
+		if (blueprint.getWestWalls().size() < 1) {
+			side = blueprint.getEastWalls().get(0);
+		} else {
+			side = blueprint.getWestWalls().get(0);
+		}
+		int baseY = side.getLowCorner().getBlockY()-1;
+		int ceilY = side.getHighCorner().getBlockY();
+		int height = ceilY - baseY - 1;
+		int maxZ = Math.max(side.getLowCorner().getBlockZ(), side.getHighCorner().getBlockZ());
+		int minZ = Math.min(side.getLowCorner().getBlockZ(), side.getHighCorner().getBlockZ());
+		int width = maxZ - minZ - 1;
+		
+		if (blueprint.getNorthWalls().size() < 1) {
 			Wall south = blueprint.getSouthWalls().get(0);
-			int maxX = Math.max(south.getLowCorner().getBlockX(), south.getHighCorner().getBlockX());
 			int minX = Math.min(south.getLowCorner().getBlockX(), south.getHighCorner().getBlockX());
 			blueprint.addFurniture(new Television(minX+3, baseY, minZ+3, width-6, height-2, BlockFace.SOUTH));
 			for (int z = minZ + 7; z <= maxZ-2; z += 3) {
-				logger.info("z = " + z + ";x = " + (minX+5) + "; width= " + (width-3));
 				blueprint.addFurniture(new Sofa(minX+5, baseY, z, width-9, BlockFace.WEST));
 			}
 		} else {
-			Wall west = blueprint.getWestWalls().get(0);
-			int baseY = west.getLowCorner().getBlockY()-1;
-			int ceilY = west.getHighCorner().getBlockY();
-			int height = ceilY - baseY - 1;
-			logger.info("baseY = " + baseY + "; ceilY = " + ceilY);
-			int maxZ = Math.max(west.getLowCorner().getBlockZ(), west.getHighCorner().getBlockZ());
-			int minZ = Math.min(west.getLowCorner().getBlockZ(), west.getHighCorner().getBlockZ());
-			int width = maxZ - minZ - 1;
-			int midZ = ((maxZ - minZ) / 2) + minZ;
-			
 			Wall north = blueprint.getNorthWalls().get(0);
-			int maxX = Math.max(north.getLowCorner().getBlockX(), north.getHighCorner().getBlockX());
 			int minX = Math.min(north.getLowCorner().getBlockX(), north.getHighCorner().getBlockX());
 			blueprint.addFurniture(new Television(minX+3, baseY, minZ+3, width-6, height-2, BlockFace.SOUTH));
 			for (int z = minZ + 7; z <= maxZ-2; z += 3) {
-				logger.info("z = " + z + ";x = " + (minX+5) + "; width= " + (width-3));
 				blueprint.addFurniture(new Sofa(minX+5, baseY, z, width-9, BlockFace.WEST));
 			}
 		}
